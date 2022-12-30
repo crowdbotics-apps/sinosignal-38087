fe6a93931aa83037c3234b0104ea0315d4b88a42
Crowdbotics <team@crowdbotics.com>
2022-12-30 00:29:47 +0000

    Initial commit.

---
+++ backend/sinosignal_38087/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sinosignal_38087 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sinosignal_38087.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sinosignal_38087",
+  url: "https://sinosignal_38087.botics.co",
+  api: "https://sinosignal_38087.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
